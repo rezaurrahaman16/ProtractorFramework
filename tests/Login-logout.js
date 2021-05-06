8ea1a55de1c1489dbeb9abcdef740999703a672d
rezaurrahaman16 <rezaurrahaman16@gmail.com>
2021-05-06 17:50:11 +0600

    Login updated code

---
+++ tests/Login-logout.js
@@ -0,0 +1,35 @@
+
+'use strict';
+var input = require('../tests/variable.js');
+// var API = require('../APIs/login.js');
+
+// describe('login with API', function () {
+//     it('Sensity user Login API', function () {
+//         var api = API;
+//         beforeAll(function () {
+//             api.login();
+//         });
+//     });
+// });
+
+describe('login screen', function () {
+    it('User login as sensity user', function () {
+        // open browser
+        browser.waitForAngularEnabled(false);
+        browser.get("https://dev.radassist.net/");
+        browser.sleep(1000);
+        element(by.id('username')).sendKeys(input.username);
+        // browser.sleep(1000);
+        element(by.id('password')).sendKeys(input.password);
+        // browser.sleep(1000);
+        element(by.xpath("//input[@id='kc-login']")).click();
+        browser.sleep(2000);
+    });
+});
+
+describe('Logout application', function () {
+    it('User Logout', function () {
+        element(by.xpath("//span[.='Logout']")).click();
+        browser.sleep(2000);
+    });
+});
---
+++ tests/variable.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var variable = {
+  "hostname":"https://dev.radassist.net/",
+  "username":"admin",
+  "password":"admin",
+};
+
+module.exports = variable;
